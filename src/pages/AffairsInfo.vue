f9e7ece8d367c0aa3f8c24362203da2fb84e22aa
Xanonymous <trusaidlin@gmail.com>
2021-03-04 08:47:49 +0800

    add top text in apge

--- src/pages/AffairsInfo.vue
+++ src/pages/AffairsInfo.vue
@@ -1,12 +1,34 @@
 <template>
-  <div>Affairs info</div>
+  <NavBar class="fixed"/>
+  <main id="affairs-info" class="page-container">
+    <div class="mt-14 lg:mt-20">
+      <p
+          class="
+          text-black
+          dark:text-white
+          text-sm
+          sm:text-lg
+          md:text-xl
+          lg:text-2xl
+          2xl:text-3xl
+          text-center
+          select-none">
+        選取資訊面向
+      </p>
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import NavBar from "@/components/app/Navbar/NavBar.vue";
+import '@/assets/scss/pages/affairs-info.scss'
 
 export default defineComponent({
-  name: "AffairsInfo"
+  name: "AffairsInfo",
+  components: {
+    NavBar
+  }
 })
 </script>
 
