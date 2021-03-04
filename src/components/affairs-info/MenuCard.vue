efd7c738b6e0030ca2506ec955d46be31660fca5
Xanonymous <trusaidlin@gmail.com>
2021-03-04 09:39:58 +0800

    finish menu-card and imported into page

--- src/components/affairs-info/MenuCard.vue
+++ src/components/affairs-info/MenuCard.vue
@@ -1,13 +1,65 @@
 <template>
-  <div></div>
+  <div
+      class="
+      menu-card
+      rounded-3xl
+      bg-black
+      dark:bg-white
+      mx-4
+      my-4
+      text-white
+      dark:text-black
+      text-center
+      inline-block
+      relative">
+    <InnerLink :to="link" class="hover:shadow-2xl">
+      <div
+          class="
+          menu-card__top-text
+          relative
+          flex
+          items-center
+          justify-center">
+        <p
+            class="
+            relative
+            font-extrabold
+            text-2xl">
+          {{ title }}
+        </p>
+      </div>
+      <div class="menu-card__img relative">
+        <LazyImg :d-src="imgUrl" class="max-h-full relative"/>
+      </div>
+    </InnerLink>
+  </div>
 </template>
 
-<script>
-export default {
-  name: "MenuCard.vue"
-}
+<script lang="ts">
+import { defineComponent, defineAsyncComponent } from 'vue'
+import '@/assets/scss/components/affairs-info/menu-card.scss'
+import InnerLink from "@/components/app/links/InnerLink.vue";
+
+export default defineComponent({
+  name: "MenuCard",
+  components: {
+    LazyImg: defineAsyncComponent(() => import('@/components/app/LazyImg.vue')),
+    InnerLink
+  },
+  props: {
+    imgUrl: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    link: {
+      type: String,
+      required: true
+    }
+  }
+})
+
 </script>
-
-<style scoped>
-
-</style>
--- src/pages/AffairsInfo.vue
+++ src/pages/AffairsInfo.vue
@@ -12,22 +12,56 @@
           lg:text-2xl
           2xl:text-3xl
           text-center
-          select-none">
+          select-none py-3">
         選取資訊面向
       </p>
+      <div class="p-4 flex flex-wrap justify-evenly">
+        <MenuCard v-for="(menuCardContent, i) of menuCardContents" :key="i"
+                  :img-url="menuCardContent.imgUrl"
+                  :link="menuCardContent.link"
+                  :title="menuCardContent.title"/>
+      </div>
     </div>
   </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive, toRefs } from 'vue'
 import NavBar from "@/components/app/Navbar/NavBar.vue";
 import '@/assets/scss/pages/affairs-info.scss'
+import MenuCard from "@/components/affairs-info/MenuCard.vue";
+import { MenuCardContent } from "@/components/affairs-info/menuCard";
 
 export default defineComponent({
   name: "AffairsInfo",
   components: {
-    NavBar
+    NavBar,
+    MenuCard
+  },
+  setup() {
+    const data = reactive({
+      menuCardContents: [
+        {
+          title: '學生面',
+          imgUrl: 'https://i.imgur.com/71nGLRO.webp',
+          link: '/'
+        },
+        {
+          title: '學務面',
+          imgUrl: 'https://i.imgur.com/71nGLRO.webp',
+          link: '/'
+        },
+        {
+          title: '教務面',
+          imgUrl: 'https://i.imgur.com/71nGLRO.webp',
+          link: '/'
+        }
+      ] as Array<MenuCardContent>
+    })
+
+    return {
+      ...toRefs(data)
+    }
   }
 })
 </script>
