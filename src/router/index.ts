9cfdf70a88b999e408c46436cabe00bc14b0d311
darkfanxing <darkfanxing@gmail.com>
2021-04-28 19:44:44 +0800

    Add redirect mainpage when error 404

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,47 +1,55 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'Home',
-    component: () => import(
-      /* webpackChunkName: "Home" */
-      /* webpackPreload: true */
-      '@/pages/Home.vue'
+    path: "/",
+    name: "Home",
+    component: () =>
+      import(
+        /* webpackChunkName: "Home" */
+        /* webpackPreload: true */
+        "@/pages/Home.vue"
       ),
   },
   {
-    path: '/about',
-    name: 'About',
-    component: () => import(
-      /* webpackChunkName: "About" */
-      /* webpackPrefetch: true */
-      '@/pages/About.vue'
-      )
+    path: "/about",
+    name: "About",
+    component: () =>
+      import(
+        /* webpackChunkName: "About" */
+        /* webpackPrefetch: true */
+        "@/pages/About.vue"
+      ),
   },
   {
-    path: '/affairs-info',
-    name: 'AffairsInfo',
-    component: () => import(
-      /* webpackChunkName: "AffairsInfo" */
-      /* webpackPrefetch: true */
-      '@/pages/AffairsInfo.vue'
-      )
+    path: "/affairs-info",
+    name: "AffairsInfo",
+    component: () =>
+      import(
+        /* webpackChunkName: "AffairsInfo" */
+        /* webpackPrefetch: true */
+        "@/pages/AffairsInfo.vue"
+      ),
   },
   {
-    path: '/chart-info',
-    name: 'ChartInfo',
-    component: () => import(
+    path: "/chart-info",
+    name: "ChartInfo",
+    component: () =>
+      import(
         /* webpackChunkName: "ChartInfo" */
         /* webpackPrefetch: true */
-        '@/pages/ChartInfo.vue'
-        )
-  }
-]
+        "@/pages/ChartInfo.vue"
+      ),
+  },
+  {
+    path: "/:pathNatch(.*)*",
+    redirect: "/"
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
